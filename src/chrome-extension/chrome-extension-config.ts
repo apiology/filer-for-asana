5c773f66b23e246d88f3149b18fb04bd38dc7304
Vince Broz <vince@broz.cc>
2022-06-08 18:36:38 -0400

    Update template

--- src/chrome-extension/chrome-extension-config.ts
+++ src/chrome-extension/chrome-extension-config.ts
@@ -18,13 +18,15 @@ const ensureConfigNotNull = <T>(value: T | null, name: string): T => {
 // You can remove this error suppression once your first config item is created
 /* eslint-disable @typescript-eslint/no-unused-vars */
 async function fetchConfig(key: string, name: string,
+  clazz: 'number'): Promise<number>;
+async function fetchConfig(key: string, name: string,
   clazz: 'string'): Promise<string>;
 async function fetchConfig(key: string, name: string,
   clazz: 'boolean'): Promise<boolean>;
 async function fetchConfig<T>(key: string, name: string,
   clazz: Class<T>): Promise<T>;
 async function fetchConfig<T>(key: string, name: string,
-  clazz: Class<T> | 'string' | 'boolean'): Promise<T | boolean | string> {
+  clazz: Class<T> | 'number' | 'string' | 'boolean'): Promise<T | boolean | string | number> {
   if (clazz === 'string') {
     const value = await chromeStorageSyncFetch(key, clazz);
     return ensureConfigNotNull(value, name);
@@ -35,16 +37,22 @@ async function fetchConfig<T>(key: string, name: string,
     return ensureConfigNotNull(value, name);
   }
 
+  if (clazz === 'number') {
+    const value = await chromeStorageSyncFetch(key, clazz);
+    console.log({ value });
+    return ensureConfigNotNull(value, name);
+  }
+
   const value = await chromeStorageSyncFetch(key, clazz);
   return ensureConfigNotNull(value, name);
 }
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
 export default class ChromeExtensionConfig {
-fetchAsanaAccessToken = async () => fetchConfig('asanaAccessToken', 'Asana access token', 'string');
+  fetchAsanaAccessToken = async () => fetchConfig('asanaAccessToken', 'Asana access token', 'string');
 
   fetchWorkspaceName = async () => fetchConfig('workspace', 'workspace name', 'string');
-// fetchSomeConfigItem = async () => fetchConfig('mumble',
+  // fetchSomeConfigItem = async () => fetchConfig('mumble',
   //                                               'example config item',
   //                                               'string');
 }
