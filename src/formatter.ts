1bd01f96a107e5a87e52aaeb14bdd71c45833854
Vince Broz <vince@broz.cc>
2022-06-08 21:39:35 -0400

    Bring back formatTask()

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,1 +1,5 @@
-export default abstract class Formatter { }
+import * as Asana from 'asana';
+
+export default abstract class Formatter {
+  abstract formatTask(task: Asana.resources.Tasks.Type): string;
+}
