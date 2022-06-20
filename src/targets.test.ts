202d8b8b18f2d8558034b6e47a24171ca4d8eb86
Vince Broz <vince@broz.cc>
2022-06-20 16:14:09 -0400

    Organize tests

--- src/targets.test.ts
+++ src/targets.test.ts
@@ -1,20 +1,31 @@
 import { prioritizedMatchedSectionTargets } from './targets.js';
 
-test('prioritizedMatchedSectionTargets', () => {
-  expect(prioritizedMatchedSectionTargets([
-    { name: 'abc' },
-  ], 'a').map((s) => s.name))
-    .toEqual(['abc']);
+describe('prioritizedMatchedSectionTargets', () => {
+  it('selects down using case-insensitive substrings', () => {
+    expect(prioritizedMatchedSectionTargets([
+      { name: 'abc' },
+    ], 'a').map((s) => s.name))
+      .toEqual(['abc']);
 
-  expect(prioritizedMatchedSectionTargets([
-    { name: 'abc' },
-    { name: 'def' },
-  ], 'a').map((s) => s.name))
-    .toEqual(['abc']);
+    expect(prioritizedMatchedSectionTargets([
+      { name: 'abc' },
+      { name: 'def' },
+    ], 'a').map((s) => s.name))
+      .toEqual(['abc']);
 
-  expect(prioritizedMatchedSectionTargets([
-    { name: 'abc' },
-    { name: 'def' },
-  ], 'A').map((s) => s.name))
-    .toEqual(['abc']);
+    expect(prioritizedMatchedSectionTargets([
+      { name: 'abc' },
+      { name: 'def' },
+    ], 'A').map((s) => s.name))
+      .toEqual(['abc']);
+  });
+
+  it('finds the most relevant matches', () => {
+    expect(prioritizedMatchedSectionTargets([
+      { name: 'ultra high [3]' },
+      { name: 'really high [3]' },
+      { name: 'high [5]' },
+    ], 'high')[0].name)
+      .toEqual('high [5]');
+  });
 });
