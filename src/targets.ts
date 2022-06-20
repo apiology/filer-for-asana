bf1d7d84d0776f3224390779cef1678e1dac87b9
Vince Broz <vince@broz.cc>
2022-06-20 16:22:50 -0400

    Move to scoring to sort suggestions by relevance (#40)
    * Organize tests
    
    * Move to scoring to sort suggestions by relevance

--- src/targets.ts
+++ src/targets.ts
@@ -6,46 +6,46 @@ export type Target = {
   section: Asana.resources.Sections.Type,
 }
 
+// penalize longer section names
+const matchScore = (
+  name: string, searchString: string
+): number => (searchString.length - name.length);
+
 export function prioritizedMatchedSectionTargets<T extends { name: string }>(
   sections: (T)[],
-  sectionName: string
+  searchString: string
 ): T[] {
-  const targets = [];
+  const scoredTargets = [];
   for (const section of sections) {
     if (section.name == null) {
       throw Error('name not included in results!');
     }
-    if (section.name.toLowerCase().includes(sectionName.toLowerCase())) {
-      targets.push(section);
+    if (section.name.toLowerCase().includes(searchString.toLowerCase())) {
+      scoredTargets.push({
+        section,
+        score: matchScore(section.name, searchString),
+      });
     }
   }
   // pick order in which to present
   //
   // ensure closest match (smallest section name used as a proxy) is first
-  targets.sort((a, b) => {
-    if (a.name == null) {
-      throw Error('name not included in results!');
-    }
-    if (b.name == null) {
-      throw Error('name not included in results!');
-    }
-    return a.name.length - b.name.length;
-  });
-  return targets;
+  scoredTargets.sort((a, b) => b.score - a.score);
+  return scoredTargets.map((st) => st.section);
 }
 
 export const addSectionsToTargets = async (
   projectGid: string,
   project: Asana.resources.Projects.Type | null,
-  sectionName: string | null,
+  searchString: string | null,
   targets: Target[]
 ): Promise<void> => {
   const client = await fetchClient();
   const sections = await client.sections.findByProject(projectGid);
 
   // 1. add each section discovered via search
-  if (sectionName != null) {
-    const sectionTargets = prioritizedMatchedSectionTargets(sections.slice(1), sectionName);
+  if (searchString != null) {
+    const sectionTargets = prioritizedMatchedSectionTargets(sections.slice(1), searchString);
     targets.push(...sectionTargets.map((section) => ({ project, section })));
   }
   // 2. add default section at end of list
