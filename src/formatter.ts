4565886e9655b8b91418e486d4db036f7812f160
Vince Broz <vince@broz.cc>
2022-06-20 20:32:16 -0400

    Improve suggestion formatting in Chrome Extension (#49)
    This avoids section running off on right side when the entered text
    is long

--- src/formatter.ts
+++ src/formatter.ts
@@ -4,4 +4,16 @@ export default abstract class Formatter {
   abstract formatTask(task: Asana.resources.Tasks.Type): string;
 
   abstract escapeDescriptionPlainText(text: string): string;
+
+  abstract formatDescriptionJustWorkspace(
+    text: string,
+    workspaceName: string
+  ): string;
+
+  abstract formatDescriptionWithSection(
+    text: string,
+    workspaceName: string,
+    projectName: string,
+    sectionName: string
+  ): string;
 }
